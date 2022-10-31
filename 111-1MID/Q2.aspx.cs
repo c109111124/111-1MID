cd89ecd3b0d6cc6540491e01cb4da23426e2c212
蔡巧柔 <100175502+c109111124@users.noreply.github.com>
2022-10-31 10:46:23 +0800

    Add files via upload

--- 111-1MID/Q2.aspx.cs
+++ 111-1MID/Q2.aspx.cs
@@ -5,17 +5,43 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace _111_1MID {
     public partial class Q2 : System.Web.UI.Page {
-        string[] s_City = new string[3] { "台北市", "新北市", "台中市" };
-        string[,] s_Area = new string[3, 3] {
+        string[] s_City = new string[2] { "台北市", "新北市" };
+        string[,] s_Area = new string[2, 3] {
             {"中正區", "文山區", "大安區"},
-            {"淡水區", "石碇區", "土城區"},
-            {"西屯區", "北屯區", "東區"}
+            {"淡水區", "石碇區", "土城區"}
         };
 
         protected void Page_Load(object sender, EventArgs e) {
+            
+            if (!IsPostBack) { 
+                for (int i_ct = 0;i_ct < s_City.Length; i_ct++)
+                {
+                    ListItem l = new ListItem();
+                    l.Text = l.Value = s_City[i_ct];
+                    dpl_City.Items.Add(l);
+                }
+                mt_secondchange();
+            }
+        }
+        protected void dpl_City_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mt_secondchange();
+        }
 
+        protected void mt_secondchange()
+        {
+            int i_index = dpl_City.SelectedIndex;
+            dpl_Area.Items.Clear();
+
+            for (int i_ct = 0; i_ct <s_Area.GetLength(1); i_ct++)
+            {
+                ListItem l = new ListItem();
+                l.Text = l.Value = s_Area[i_index, i_ct];
+                dpl_Area.Items.Add(l);
+            }
         }
     }
 }
--- 111-1MID/Q2_Sub.aspx.cs
+++ 111-1MID/Q2_Sub.aspx.cs
@@ -5,10 +5,15 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace _111_1MID {
     public partial class Q2_Sub : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
 
+            lb_msg.Text = "紀錄編號: " + Request.Form.Get("tb_Num") + "<br />" + "通訊種類: " + Request.Form.Get("rbl_Phone") + "<br />" + "通訊號碼: " + Request.Form.Get("txt_Phone") + "<br />" + "所在城市: " + Request.Form.Get("dpl_City") + "<br />" + "所在區域: " + Request.Form.Get("dpl_Area") + "<br />";
+            
+
+            
         }
     }
 }
